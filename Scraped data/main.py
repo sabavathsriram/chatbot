8da47f99239fb931f896954f660dec7cf19f1d9d
sabavathsriram <sabavathsreeram242@gmail.com>
2025-05-29 20:06:17 +0530

    Delete Scraped data directory

--- Scraped data/main.py
+++
@@ -1,7 +0,0 @@
-import requests
-def fetchAndSaveToFile(url,path):
-    r=requests.get(url)
-    with open(path,"w") as f:
-        f.write(r.text)
-url="https://www.kmit.in/"
-fetchAndSaveToFile(url,"data/Dataset.html")
